ad9b63fe7f5246f4dec733c535d4b9ae4d80dfb4
Yihun Alemayehu <yankure01@gmail.com>
2023-11-26 13:32:57 +0300

    Initial commi

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      useMaterial3: true,
+    ),
+    home: const IntroPage(),
+  ));
+}
